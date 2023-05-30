a9b9ee3cdfdd687b3f69200352100fbdbd8667e0
Kirilas <sersniovask@gmail.com>
2023-05-30 08:45:36 +0300

    Rezultatu failas

--- AT_TEST/src/write_commands.py
+++ AT_TEST/src/write_commands.py
@@ -5,8 +5,8 @@ from datetime import datetime
 def write_to_file(rows, hostname):
     fields = ["command", "result"]
     now = datetime.now()
-    date = now.strftime(f"%d'th of %b %Y_%X")
-    with open(f"test_results_{hostname}_{date}.csv", "w", newline='') as file:
+    date = now.strftime(f"%d'th of %b %Y-%X")
+    with open(f"results/test_results_{hostname}_{date}.csv", "w", newline='') as file:
         writer = csv.DictWriter(file, fieldnames= fields)
         writer.writeheader()
         writer.writerows(rows)
